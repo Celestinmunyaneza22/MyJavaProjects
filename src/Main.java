6cfa4d2d0f34da811cc2f347b758db1ac95f1229
celestin <celestin@gmail.com>
2025-10-16 06:59:05 +0200

    data types,recieving input from keyboard

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
      public static void main(String[] args){
        System.out.print("Hello world!");
+       System.out.println("Welcome in java development");
+       System.out.println("Enjoy your time with coding!");
+
+       int age=45;
+       System.out.println(age);
+
+       int num=10;
+       if(num>0){
+           System.out.println("number is positive");
+       }else{
+           System.out.println("number is negative");
+       }
     }
 }
